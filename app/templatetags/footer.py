8797a7f5848e32873b099a63c4cba2622321b9a8
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2024-03-26 18:08:35 +0500

    fix some bugs

--- app/templatetags/footer.py
+++ app/templatetags/footer.py
@@ -8,7 +8,7 @@ register = template.Library()
 def footer(context) -> dict[str, any]:
 
     query = Menu.objects.filter(is_active=True, language__name=context['request'].LANGUAGE_CODE).first()
-    blogs = Blog.objects.order_by('created_at')[:2]
+    blogs = Blog.objects.filter(language__name=context['request'].LANGUAGE_CODE).order_by('created_at')[:2]
 
     return {
         'info': query,
